df9673249f7f939dc89af1d1ccc9ebeeae74e96b
RoshTzsche <roshguadiana@gmail.com>
2026-03-06 22:20:21 -0600

    canny filter v0.4, nms initialization

--- src/vision/canny_filter.py
+++ src/vision/canny_filter.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn.functional as F
-
+# Non-Maximum Suppression (NMS) is a method used in object detection to remove extra boxes that are detected around the same object. When an object is detected multiple times with different bounding boxes, NMS keeps the best one and removes the rest. This helps us to make sure each object is counted only once, improving the accuracy and clarity of the results.
 def canny(image_tensor):
     """
-    Recycled function from filter_sobel, but with a twist, I will transform the function
+    Recycled function from filter_sobel, with a twist, I will transform the function
     into a canny filter, using the arctan2 funcion.
     The arctan2 function has as input two vectors, and the output is the angle between them
     in Radians, you could investigate more in deep, but it derives from linear algebra and the 
@@ -34,8 +34,6 @@ def canny(image_tensor):
     G_x = F.conv2d(image_tensor, kernel_x, padding=1)
     G_y = F.conv2d(image_tensor, kernel_y, padding=1)
     
-
-
     # Gradient Magnitude (Euclidean Norm)
     magnitude = torch.sqrt(G_x**2 + G_y**2)
     angle_rad = torch.atan2(G_y, G_x)
@@ -44,5 +42,16 @@ def canny(image_tensor):
     
     # Output the complete spatial derivative state
     return G_x, G_y, magnitude, angle_deg
-    return magnitude
  
+'''Next we will do a Non-Maximum Suppression logic, using the torch.roll function
+This function its like moving some dimension of the tensor x places 
+This created shifted copies of the original matrix
+For example, defining dim=3 as the columns, horizontal, width or x axis, if you shift the tensor Right (shifts=1, dims=3), the pixel currently residing at index (i,j) in the new tensor is the Left Neighbor M(i,j−1) from the original tensor.
+I won't use a single for loop, because this is slow, we need to use some vectorized operations, and making some boolean masks
+
+'''
+def NMS(image_tensor):
+    
+    degree_matrix = canny(image_tensor)[3]
+    Mask_0_deg = (angle_deg == 0)
+
